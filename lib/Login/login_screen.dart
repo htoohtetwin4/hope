0e3aa31bfd22af409f9c20f39bde372b86e40912
htoohtetwin4 <htoohtetwin.hhw4@gmail.com>
2024-02-23 19:17:23 +0630

    hope application

---
+++ lib/Login/login_screen.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'package:hope_app/Login/sign_up_screen.dart';
+import 'package:hope_app/constants.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        // padding: EdgeInsets.symmetric(vertical: 40),
+        width: double.infinity,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+          colors: [Color(0xffbbd2c5), Color(0xFFA4C639), Color(0xFF81B93D)],
+          stops: [0, 0.5, 1],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        )),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 150),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    "Hope",
+                    style: TextStyle(color: Colors.white, fontSize: 50),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            //login space
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                    color: hSecondaryColor,
+                    borderRadius:
+                        BorderRadius.only(topLeft: Radius.elliptical(300, 70))),
+                //login box
+
+                child: Padding(
+                  padding: EdgeInsets.all(50),
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(
+                        height: 50,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: hSecondaryColor,
+                            borderRadius: BorderRadius.circular(20),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: hBaseColorOne,
+                                  offset: Offset(0, 1),
+                                  blurRadius: 10)
+                            ]),
+                        child: Padding(
+                            padding: EdgeInsets.all(20),
+                            child: Column(
+                              children: <Widget>[
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                TextField(
+                                  decoration: InputDecoration(
+                                    focusedBorder: UnderlineInputBorder(
+                                        borderSide:
+                                            BorderSide(color: Colors.green)),
+                                    hintText: 'Username',
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                TextField(
+                                  decoration: InputDecoration(
+                                    focusedBorder: UnderlineInputBorder(
+                                        borderSide:
+                                            BorderSide(color: Colors.green)),
+                                    hintText: 'Password',
+                                  ),
+                                ),
+                              ],
+                            )),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Container(
+                          height: 50,
+                          margin: EdgeInsets.symmetric(horizontal: 50),
+                          decoration: BoxDecoration(
+                            color: hBaseColorThree,
+                          ),
+                          child: Center(
+                            child: Text("Login",
+                                style: TextStyle(
+                                    color: hSecondaryColor, fontWeight: fw_9)),
+                          )),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Center(
+                        child: Row(
+                          children: [
+                            Expanded(child: Text("forgot password?")),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const SignUpScreen()));
+                              },
+                              child: Text(
+                                "Register Now",
+                                style: TextStyle(
+                                    color: hBaseColorThree, fontWeight: fw_9),
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/constants.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+//Color
+const hBaseColorOne = Color(0xffbbd2c5);
+const hBaseColorTwo = Color(0xFFA4C639);
+const hBaseColorThree = Color(0xFF81B93D);
+// const hBaseColorTwo = Color(0xff536976);
+// const hBaseColorThree = Color(0xff292e49);
+const hSecondaryColor = Color.fromRGBO(231, 238, 241, 1);
+
+//Fonts
+const fontNormal = FontStyle.normal;
+const fw_9 = FontWeight.w900;
+const fw_8 = FontWeight.w800;
+const fw_7 = FontWeight.w700;
+const fw_6 = FontWeight.w600;
+const fw_5 = FontWeight.w500;
+const fw_4 = FontWeight.w400;
+const fw_3 = FontWeight.w300;
+const fw_2 = FontWeight.w200;
+const fw_1 = FontWeight.w100;
