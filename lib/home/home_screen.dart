dddc45d0fcf2cd0aac90cb81b361ea4dd09371e2
htoohtetwin4 <htoohtetwin.hhw4@gmail.com>
2024-02-24 00:30:55 +0630

    added signup screen with validation

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hope_app/constants.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,8 +11,26 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("hello"),
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [hBaseColorOne, hBaseColorTwo, hBaseColorThree],
+              stops: [0, 0.5, 1],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+        ),
+        title: Text(
+          "Hope",
+          style: TextStyle(color: Colors.white, fontWeight: fw_6, fontSize: 30),
+        ),
+      ),
+      body: Container(
+        child: Text("Home"),
+      ),
     );
   }
 }
